96430a6b06e0e0a723f7cd1740550453b6d6137d
Alexandra Bystrovskaya <bystrowzki@inbox.ru>
2021-06-22 18:45:45 +0600

    Files added

---
+++ files2.py
@@ -0,0 +1,32 @@
+import pprint
+
+cook_book = {}
+
+file = open("recipes.txt", "r", encoding="utf-8")
+for line in file:
+    name = line.strip()
+    number = int(file.readline().strip())
+    comp = []
+    for item in range(number):
+        ingredients = {}
+        ing = file.readline().split("|")
+        ingredients["ingredient_name"] = ing[0]
+        ingredients["quantity"] = ing[1]
+        ingredients["measure"] = ing[2].strip()
+        comp.append(ingredients)
+    f = file.readline().strip()
+    cook_book[name] = comp
+
+cooking_list = {}
+def get_shop_list_by_dishes(dishes, person_count):
+    for dish in dishes:
+        if dish in cook_book.keys():
+            for items in cook_book[dish]:
+                amount = {}
+                cooking_list[items['ingredient_name']] = amount
+                amount['measure'] = items['measure']
+                amount['quantity'] = int(items['quantity'])*person_count
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+pprint.pprint(cooking_list)
